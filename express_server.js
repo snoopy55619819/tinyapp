21ae8d18c378a4247abcb345bae7ebf5125427fd
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-12-07 22:43:55 +0000

    create new ejs template and add route to server

--- express_server.js
+++ express_server.js
@@ -26,6 +26,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  
+  const templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
