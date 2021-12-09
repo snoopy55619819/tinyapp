ba5434062011aa5e9a65559627162025d8c25359
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-12-09 04:14:48 +0000

    require cookie-parser in server and use in middleware.

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
